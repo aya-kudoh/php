2fe28a6825e043951f8f6b05ea9167965be09298
aya-kudoh <a1021bn@outlook.jp>
2020-11-13 13:14:10 +0000

    改行

--- 3.4.php
+++ 3.4.php
@@ -1,9 +1,14 @@
 <?php
 $numlist = ["one","two","three"];
-print($numlist[1]);
+#print($numlist[1]);
 
 $stringlist = [];
 array_push($stringlist,"samurai");
-print($stringlist[0]);
+#print($stringlist[0]);
+echo($numlist[1]);
+
+echo("\n");
+
+echo($stringlist[0]);
 
 ?>

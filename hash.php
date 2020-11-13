88afb7357cb970761d240ae60512a6dc15986a8e
aya-kudoh <a1021bn@outlook.jp>
2020-11-13 12:25:16 +0000

    データを追加

--- hash.php
+++ hash.php
@@ -4,9 +4,22 @@ $date = [
     "gender"=>"男性",
     "age"=>46
 ];
+$date["address"] = "東京";
+
+var_dump($date);
 
 print($date["name"]);
 print($date["gender"]);
 print($date["age"]);
+print($date["address"]);
+
+$date["gender"] = "女性";
+var_dump($date);
+
+$date["children"] = ["太郎","次郎","三郎"];
+var_dump($date);
+
+var_dump($date["children"]);
+
 
 ?>

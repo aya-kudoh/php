db17dedcfcf4ef1a620207860ddb242a498b5ed4
aya-kudoh <a1021bn@outlook.jp>
2020-11-13 13:07:03 +0000

    最終課題step4

--- restaurants_searcher.php
+++ restaurants_searcher.php
@@ -1,6 +1,12 @@
-# restaurants_searcher.php
-
 <?php
-$restaurants = [];
-$response = "hugahuga";
+function write_date_to_csv(){
+    $restaurants = [];
+    $response = "hugahuga";
+    
+    return var_dump($restaurants);
+}
+
+
+
+write_date_to_csv();
 ?>

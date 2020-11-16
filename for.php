c06a6d8c98359169581ff21d1a91b78c2c6f1fe1
aya-kudoh <a1021bn@outlook.jp>
2020-11-16 11:14:20 +0000

    PHP7章

---
+++ for.php
@@ -0,0 +1,25 @@
+<?php
+for ($i = 1; $i <=10; $i++){
+    print("Hello World"."\n");
+    
+}
+
+$animals = ["dog", "cat", "bird"];
+foreach ($animals as $animals){
+    print($animals. "\n");
+}
+
+for ($i = 1; $i < 31; $i++){
+    if($i % 3 == 0){
+        print("三の倍数です！"."\n");
+    }else{
+        print($i."\n");
+    }
+}
+
+
+
+
+
+
+?>

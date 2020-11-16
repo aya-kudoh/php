79a1f8e61d960bc91142e7d94be35d07c56c4222
aya-kudoh <a1021bn@outlook.jp>
2020-11-16 11:53:14 +0000

    class.php8.2まで

---
+++ class.php
@@ -0,0 +1,21 @@
+<?php
+class car
+{
+    static $class_name = "Car";
+    
+    function init(){
+        $this->name = "";
+    }
+    
+    public function show()  {
+        print($this->name."\n");
+    }
+}
+
+$car = new car();
+$car->name = "セダン";
+$car->show();
+print(car::$class_name);
+
+
+?>

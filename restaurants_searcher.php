f082d2f9144edf653987b5ad2f2a4f90c09bee91
aya-kudoh <a1021bn@outlook.jp>
2020-11-16 11:04:07 +0000

    最終課題5

--- restaurants_searcher.php
+++ restaurants_searcher.php
@@ -1,12 +1,20 @@
 <?php
 function write_date_to_csv(){
-    $restaurants = [];
-    $response = "hugahuga";
     
-    return var_dump($restaurants);
+    $restaurants = [];
+    $response = "hugahoge";
+    
+    if(isset($response["error"])){
+    return print("エラーが発生しました");
+    }
+    
+    return print_r($restaurants);
+    
 }
 
 
 
 write_date_to_csv();
+
+
 ?>

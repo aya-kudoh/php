af54fe8208e7938100dacc1532c238ee0e53c823
aya-kudoh <a1021bn@outlook.jp>
2020-11-17 15:13:27 +0000

    php8.3classを作ってみよう

---
+++ class2.php
@@ -0,0 +1,21 @@
+<?php
+class Human
+{
+    static $class_name = "Human";
+    
+    function init(){
+        $this->name = "大泉";
+    }
+    
+    public function show(){
+        print($this->name."\n");
+    }
+}
+
+print(Human::$class_name);
+
+
+
+
+
+?>
